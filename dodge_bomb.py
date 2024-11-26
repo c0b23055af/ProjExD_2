6c4f7c5e1fc88566863e35715d51d410003f207f
kumagai <c0b23055af@edu.teu.ac.jp>
2024-11-26 14:50:09 +0900

    練習4：衝突判定

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -45,6 +45,10 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+            
+        if kk_rct.colliderect(bb_rct):
+            print("game over")
+            return #ゲームオーバー
         screen.blit(bg_img, [0, 0]) 
 
         key_lst = pg.key.get_pressed()
